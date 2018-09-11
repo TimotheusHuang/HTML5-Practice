1e24c0e1516f4a17ead99c84fd3a5921cddfb9ff
TimotheusHuang <huangtim30@gmail.com>
2018-09-11 15:53:28 +0800

    mailto modified

--- email.php
+++ email.php
@@ -25,23 +25,23 @@
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
- 
+    if(!preg_match($email_exp,$email_from)) {
+        $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+    }
+
     $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$name)) {
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(strlen($message) < 2) {
-    $error_message .= 'The message you entered do not appear to be valid.<br />';
-  }
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
+
+    if(!preg_match($string_exp,$name)) {
+        $error_message .= 'The Name you entered does not appear to be valid.<br />';
+    }
+
+    if(strlen($message) < 2) {
+        $error_message .= 'The message you entered do not appear to be valid.<br />';
+    }
+
+    if(strlen($error_message) > 0) {
+        died($error_message);
+    }
  
     $email_message = "Form details below.\n\n";
 
@@ -60,9 +60,19 @@
     $headers = 'From: '.$email_from."\r\n".
     'Reply-To: '.$email_from."\r\n" .
     'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);  
+//    @mail($email_to, $email_subject, $email_message, $headers);  
+?>
+    <script>
+        var name = '<?php echo $name ?>';
+        var email = '<?php echo $email ?>';
+        var email_message = '<?php echo $email_message ?>';
+        var email_sub = '<?php echo $email_subject ?>';
+        var email_str = 'mailto:' + email + '?subject=' + email_sub + '&body=' + email_message;
+        window.open(String(email_str).replace('^', '@') );
+    </script>
+<?php
     }
-    
+   
     header("Location:index.php");
 ?>
 
